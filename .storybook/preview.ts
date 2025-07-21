dbbbe1cebe9ca26aa84c766d0df6562772ecb766
rui@trevor.io <rui@embeddable.com>
2025-07-21 16:50:58 +0100

    include css in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from '@storybook/react-webpack5';
+import { injectCssVariables } from '../src/theme/theme.utils';
+import { theme } from '../src/theme/theme';
+
+injectCssVariables(theme);
 
 const preview: Preview = {
   parameters: {
