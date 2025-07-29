6a2321af6147c2bd69e51f52fa27110c889fea4e
rui@trevor.io <rui@embeddable.com>
2025-07-29 16:25:20 +0100

    remove storybook blockers

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,7 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-webpack5-compiler-swc',
-    '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
-  ],
+  addons: ['@storybook/addon-webpack5-compiler-swc'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
