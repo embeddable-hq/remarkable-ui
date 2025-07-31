f1c8cd3308ac01fb24f29b99f943a66ddc0587a3
rui@trevor.io <rui@embeddable.com>
2025-07-31 13:23:29 +0100

    update translation files

--- src/theme/theme-formatter/theme-formatter-translations/de.ts
+++ src/theme/theme-formatter/theme-formatter-translations/de.ts
@@ -2,14 +2,33 @@ import { ResourceLanguage } from 'i18next';
 
 export const de: ResourceLanguage = {
   translation: {
-    common: {
-      other: 'Andere',
+    dimension: {
+      'customers.country.United States': 'Vereinigte Staaten',
+      'customers.country.Germany': 'Deutschland',
+      'customers.country.United Kingdom': 'Vereinigtes Königreich',
+      'customers.country.New Zealand': 'Neuseeland',
+      'customers.country.Belgium': 'Belgien',
+      'customers.country.Australia': 'Australien',
+      'customers.country.Canada': 'Kanada',
+      'customers.country.Iceland': 'Island',
+      'United States': 'Vereinigte Staaten',
+      Germany: 'Deutschland',
+      'United Kingdom': 'Vereinigtes Königreich',
+      'New Zealand': 'Neuseeland',
+      Belgium: 'Belgien',
+      Australia: 'Australien',
+      Canada: 'Kanada',
+      Iceland: 'Island',
+      Other: 'Andere',
     },
     charts: {
       errorTitle: 'Etwas ist schiefgelaufen.',
       errorMessage: 'Beim Laden des Diagramms ist ein Fehler aufgetreten.',
       emptyTitle: 'Hier ist es etwas leer.',
       emptyMessage: 'Versuchen Sie, etwas hinzuzufügen.',
+    },
+    granularity: {
+      quarter: '{{quarter}}. Quartal {{year}}',
     },
   },
 };
--- src/theme/theme-formatter/theme-formatter-translations/en.ts
+++ src/theme/theme-formatter/theme-formatter-translations/en.ts
@@ -2,14 +2,14 @@ import { ResourceLanguage } from 'i18next';
 
 export const en: ResourceLanguage = {
   translation: {
-    common: {
-      other: 'Other',
-    },
     charts: {
       errorTitle: 'Something went wrong.',
       errorMessage: 'An error occurred while loading the chart.',
       emptyTitle: "It's a bit empty here.",
       emptyMessage: 'Try adding something.',
+    },
+    granularity: {
+      quarter: 'Q{{quarter}} {{year}}',
     },
   },
 };
