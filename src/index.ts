b2eaf60e7e1162e1fbf7920c569e0dfa8cc94a52
rui@trevor.io <rui@embeddable.com>
2025-08-12 20:32:38 +0100

    remove index file

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { defaultChartsMenuOptions } from './remarkable-ui-embeddables/theme/theme.constants';
-export * from './remarkable-ui-embeddables/theme/theme.types';
-export * from './remarkable-ui-embeddables/theme/theme.constants';
