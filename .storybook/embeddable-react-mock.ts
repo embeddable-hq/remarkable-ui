3fc329ac7b0a7f8650b12a5af516e4dd6d2ece17
rui@trevor.io <rui@embeddable.com>
2025-08-26 18:52:31 +0100

    Updates

---
+++ .storybook/embeddable-react-mock.ts
@@ -0,0 +1,4 @@
+export * from '@embeddable.com/react';
+import { remarkableTheme } from '../src/remarkable-ui-embeddables/theme/theme.constants';
+
+export const useTheme = () => remarkableTheme;
---
+++ .storybook/preview.ts
@@ -0,0 +1,47 @@
+import type { Preview } from '@storybook/react';
+import { injectCssVariables } from '../src/remarkable-ui-embeddables/theme/styles/styles.utils';
+import { remarkableTheme } from '../src/remarkable-ui-embeddables/theme/theme.constants';
+
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', title: 'English' },
+        { value: 'de', title: 'German' },
+      ],
+    },
+  },
+};
+
+// TODO: Make storybook work with i18n - on language change
+// i18nSetup(remarkableTheme);
+// export const decorators = [
+//   (Story, context) => {
+//     const { locale } = context.globals;
+
+//     if (i18n.language !== locale) {
+//       i18n.changeLanguage(locale);
+//     }
+
+//     return Story();
+//   },
+// ];
+
+injectCssVariables(remarkableTheme.styles);
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
