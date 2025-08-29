b87dfbb6d9dce0ea676ff3595cbfd1390c45b748
rui@trevor.io <rui@embeddable.com>
2025-08-29 17:32:14 +0100

    update index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export { i18n } from './remarkable-pro/theme/i18n/i18n';
 export * from './remarkable-pro/theme/theme.types';
 export * from './remarkable-pro/theme/theme.constants';
 export type {
@@ -7,3 +8,6 @@ export type {
   ThemeFormatter,
 } from './remarkable-pro/theme/formatter/formatter.types';
 export * from './remarkable-pro/types/deep-partial';
+
+// Temporary until we extract remarkable-ui to its own package
+export * from './remarkable-ui';
