0965ac0ec4f35a2995223375758f8c05468214de
rui@trevor.io <rui@embeddable.com>
2025-09-01 19:18:11 +0100

    RUI-000: add embeddable.config file

---
+++ .changeset/slimy-tips-cut.md
@@ -0,0 +1,5 @@
+---
+'@embeddable.com/remarkable-ui': patch
+---
+
+Include embeddable.config file
---
+++ embeddable.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@embeddable.com/sdk-core';
+import react from '@embeddable.com/sdk-react';
+
+export default defineConfig({
+  plugins: [react],
+  lifecycleHooksFile: './embeddable.lifecycle.ts',
+  pushModels: false,
+});
