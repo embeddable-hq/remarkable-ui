ddc466e3dd8578b74801bad0637c84deabef7bc1
mikhail-terekhov <mikhail@embeddable.de>
2025-09-01 10:52:12 +0200

    RUI-000: Test: Remove @storybook/addon-webpack5-compiler-swc (#47)
    * Test: Remove @storybook/addon-webpack5-compiler-swc
    
    * changeset
    
    * uninstall addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-webpack5-compiler-swc'],
+  // addons: ['@storybook/addon-webpack5-compiler-swc'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
