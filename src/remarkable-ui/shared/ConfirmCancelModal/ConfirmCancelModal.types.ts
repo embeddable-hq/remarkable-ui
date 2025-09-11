2b2a4c92b00e05ccec68806638c9127576cd1964
Ako Hosseini <alihk12@mail.com>
2025-09-11 18:29:35 +0200

    chore: clean up

--- src/remarkable-ui/shared/ConfirmCancelModal/ConfirmCancelModal.types.ts
+++ src/remarkable-ui/shared/ConfirmCancelModal/ConfirmCancelModal.types.ts
@@ -1,16 +1,9 @@
 export type ConfirmCancelModalProps = {
-  /** Title of the modal */
   title: string;
-  /** Message content of the modal */
   message: string;
-  /** Label for the confirm button */
   confirmLabel?: string;
-  /** Label for the cancel button */
   cancelLabel?: string;
-  /** Function to call when confirm button is clicked */
   onConfirm: () => void;
-  /** Function to call when cancel button is clicked */
   onCancel?: () => void;
-  /** Additional CSS class name */
   className?: string;
 };
