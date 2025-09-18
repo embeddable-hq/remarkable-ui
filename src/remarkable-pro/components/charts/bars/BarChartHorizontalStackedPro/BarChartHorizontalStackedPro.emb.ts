0a4020a57df7e2ab94701f5b1e53cfca5cb01ccc
Rui Ribeiro <57071299+mad-raccoon@users.noreply.github.com>
2025-09-18 13:52:45 +0200

    R UI 79 bar stacked chart pro (#121)
    * RUI-86: stacked bar variants
    
    * remove staked none
    
    * phase 1: visual improvements + add required inputs for the stacked
    
    * barchart small improvements
    
    * disable stacked100 for now
    
    * clean up
    
    * sort inputs
    
    * fix storybook reverse axis on the bar chart
    
    * bar chart rename + create new grouped variants
    
    * remove comments
    
    * mr review
    
    * improve the segment selection
    
    ---------
    
    Co-authored-by: R <r@unknown8a159e9269e7.lan>

---
+++ src/remarkable-pro/components/charts/bars/BarChartHorizontalStackedPro/BarChartHorizontalStackedPro.emb.ts
@@ -0,0 +1,76 @@
+import { Value, loadData } from '@embeddable.com/core';
+import { defineComponent, EmbeddedComponentMeta, Inputs } from '@embeddable.com/react';
+import BarChartHorizontalStackedPro from './index';
+import {
+  dataset,
+  description,
+  dimension,
+  showLegend,
+  showTooltips,
+  showValueLabels,
+  title,
+  measure,
+  showLogarithmicScale,
+  xAxisLabel,
+  yAxisLabel,
+  showTotalLabels,
+  reverseYAxis,
+  xAxisRangeMin,
+  xAxisRangeMax,
+} from '../../../component.constants';
+
+export const meta = {
+  name: 'BarChartHorizontalStackedPro',
+  label: 'Bar Chart Horizontal Stacked',
+  category: 'Bar Charts',
+  inputs: [
+    dataset,
+    measure,
+    { ...dimension, name: 'yAxis', label: 'Y-axis' },
+    { ...dimension, name: 'groupBy', label: 'Group by' },
+    title,
+    description,
+    showLegend,
+    showTooltips,
+    { ...showValueLabels, defaultValue: false },
+    showLogarithmicScale,
+    xAxisLabel,
+    yAxisLabel,
+    reverseYAxis,
+    xAxisRangeMin,
+    xAxisRangeMax,
+    showTotalLabels,
+  ],
+  events: [
+    {
+      name: 'onSegmentClick',
+      label: 'A bar is clicked',
+      properties: [
+        {
+          name: 'dimensionValue',
+          label: 'Clicked Dimension value',
+          type: 'string',
+        },
+      ],
+    },
+  ],
+} as const satisfies EmbeddedComponentMeta;
+
+export default defineComponent(BarChartHorizontalStackedPro, meta, {
+  props: (inputs: Inputs<typeof meta>) => {
+    return {
+      ...inputs,
+      results: loadData({
+        from: inputs.dataset,
+        select: [inputs.yAxis, inputs.groupBy, inputs.measure],
+      }),
+    };
+  },
+  events: {
+    onSegmentClick: (value) => {
+      return {
+        dimensionValue: value.dimensionValue || Value.noFilter(),
+      };
+    },
+  },
+});
