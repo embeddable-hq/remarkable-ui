90809bfda9b279bfedc2f67d5c91cfc9a0180c8a
mad-raccoon <ribeiro.ruifigueiredo@gmail.com>
2025-10-01 13:16:21 +0200

    format currency fix

--- src/remarkable-pro/theme/formatter/formatter.constants.ts
+++ src/remarkable-pro/theme/formatter/formatter.constants.ts
@@ -64,7 +64,7 @@ const dataNumberFormatter = (theme: Theme, key: DimensionOrMeasure): NumberForma
     style: currency ? 'currency' : undefined,
     currency: currency ? currency : undefined,
     notation: key.inputs?.abbreviateLargeNumber ? 'compact' : undefined,
-    maximumFractionDigits: key.inputs?.decimalPlaces ?? undefined,
+    maximumFractionDigits: currency ? (key.inputs?.decimalPlaces ?? 0) : undefined,
   };
 
   return theme.formatter.numberFormatter(theme, options);
