c4cb35f381779059ee0a4eb25bae1f2da2d455d6
Ako Hosseini <alihk12@mail.com>
2025-10-28 16:48:56 +0100

    feat: use typography

--- src/remarkable-ui/shared/GhostButton/GhostButton.tsx
+++ src/remarkable-ui/shared/GhostButton/GhostButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Icon } from '@tabler/icons-react';
 import clsx from 'clsx';
 import styles from './GhostButton.module.css';
+import { Typography } from '../Typography/Typography';
 
 export type GhostButtonProps = {
   startIcon?: Icon;
@@ -17,9 +18,9 @@ export const GhostButton: React.FC<GhostButtonProps> = ({
 }) => {
   return (
     <button className={clsx(styles.ghostButton, className)} {...props}>
-      {StartIcon && <StartIcon className={styles.startIcon} />}
-      {children}
-      {EndIcon && <EndIcon className={styles.endIcon} />}
+      {StartIcon && <StartIcon className={styles.icon} />}
+      <Typography className={styles.label}>{children}</Typography>
+      {EndIcon && <EndIcon className={styles.icon} />}
     </button>
   );
 };
