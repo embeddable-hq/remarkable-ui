d85c1859adcef1afd6f49dd5c4d008838f98c5e7
Rui Ribeiro <57071299+mad-raccoon@users.noreply.github.com>
2025-12-01 15:15:49 +0000

    RUI-153: separate RUI from RPRO (#250)
    * RUI-153: separate RUI from RPRO
    
    * Update storybook files
    
    * remove not needed packages + clean up storybook files
    
    * Rollback version
    
    * Fix MR remarks
    
    * Fix MR remarks

---
+++ src/components/charts/pies/PieChart/PieChart.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+import { PieChart } from './PieChart';
+import { pieDataMock } from '../pies.mock';
+
+const meta = {
+  title: 'Charts/PieChart',
+  component: PieChart,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: 250,
+          height: 250,
+          display: 'flex',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    data: pieDataMock,
+  },
+} satisfies Meta<typeof PieChart>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/components/shared/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta } from '@storybook/react-webpack5';
+
+import { Skeleton } from './Skeleton';
+
+const meta = {
+  title: 'Shared/Skeleton',
+  component: Skeleton,
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+
+export const Default = () => {
+  return (
+    <div style={{ width: '200px', height: '100px' }}>
+      <Skeleton />
+    </div>
+  );
+};
+
+export const Resize = () => {
+  return (
+    <div style={{ width: '200px', height: '100px', resize: 'both', overflow: 'auto' }}>
+      <Skeleton />
+    </div>
+  );
+};
