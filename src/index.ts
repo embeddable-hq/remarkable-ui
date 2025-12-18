739f086daf994957fdd72b72b9146cea5dcd4c4e
Rui Ribeiro <57071299+mad-raccoon@users.noreply.github.com>
2025-12-18 15:31:44 +0000

    RUI-137: date range picker css vars (#284)
    * RUI-137: date range picker css vars
    
    * clean up index.ts
    
    * update css variables

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,61 @@
 // Shared
-export { Skeleton } from './components/shared/Skeleton/Skeleton';
-export { Card, CardContent, CardHeader } from './components/shared/Card/Card';
-export { CardFeedback } from './components/shared/Card/CardFeedback/CardFeedback';
-export { Typography } from './components/shared/Typography/Typography';
-export { ActionIcon } from './components/shared/ActionIcon/ActionIcon';
-export { ButtonIcon } from './components/shared/ButtonIcon/ButtonIcon';
-export { Dropdown } from './components/shared/Dropdown/Dropdown';
-export {
-  SelectListOption,
-  type SelectListOptionProps,
-} from './components/editors/selects/shared/SelectFieldContent/SelectFieldOptions/SelectFieldOption/SelectFieldOption';
-export { Button } from './components/shared/Button/Button';
-export { GhostButton } from './components/shared/GhostButton/GhostButton';
-export { Overlay } from './components/shared/Overlay/Overlay';
-export { MultiSelectField } from './components/editors/selects/MultiSelectField/MultiSelectField';
-export { Switch } from './components/editors/Switch/Switch';
-export { FieldFeedback } from './components/shared/Field/FieldFeedback';
-export { FieldHeader } from './components/shared/Field/FieldHeader';
+export * from './components/shared/Skeleton/Skeleton';
+export * from './components/shared/Card/Card';
+export * from './components/shared/Card/CardFeedback/CardFeedback';
+export * from './components/shared/Typography/Typography';
+export * from './components/shared/ActionIcon/ActionIcon';
+export * from './components/shared/ButtonIcon/ButtonIcon';
+export * from './components/shared/Dropdown/Dropdown';
+export * from './components/editors/selects/shared/SelectFieldContent/SelectFieldOptions/SelectFieldOption/SelectFieldOption';
+export * from './components/shared/Button/Button';
+export * from './components/shared/GhostButton/GhostButton';
+export * from './components/shared/Overlay/Overlay';
+export * from './components/editors/selects/MultiSelectField/MultiSelectField';
+export * from './components/editors/Switch/Switch';
+export * from './components/shared/Field/FieldFeedback';
+export * from './components/shared/Field/FieldHeader';
 
 // Editors
-export { SelectFieldTrigger } from './components/editors/selects/shared/SelectFieldTrigger/SelectFieldTrigger';
-export {
-  SelectFieldContent,
-  SelectFieldContentList,
-} from './components/editors/selects/shared/SelectFieldContent/SelectFieldContent';
-export { SingleSelectField } from './components/editors/selects/SingleSelectField/SingleSelectField';
-export { TextField } from './components/editors/inputs/TextField/TextField';
-export { NumberField } from './components/editors/inputs/NumberField/NumberField';
-export { SelectFieldCategory } from './components/editors/selects/shared/SelectFieldContent/SelectFieldOptions/SelectFieldCategory/SelectFieldCategory';
-export { DateRangePicker } from './components/editors/dates/DateRangePicker/DateRangePicker';
-export { DateRangePickerField } from './components/editors/dates/DateRangePickerField/DateRangePickerField';
+export * from './components/editors/selects/shared/SelectFieldTrigger/SelectFieldTrigger';
+export * from './components/editors/selects/shared/SelectFieldContent/SelectFieldContent';
+export * from './components/editors/selects/SingleSelectField/SingleSelectField';
+export * from './components/editors/inputs/TextField/TextField';
+export * from './components/editors/inputs/NumberField/NumberField';
+export * from './components/editors/selects/shared/SelectFieldContent/SelectFieldOptions/SelectFieldCategory/SelectFieldCategory';
+export * from './components/editors/dates/DateRangePicker/DateRangePicker';
+export * from './components/editors/dates/DateRangePickerField/DateRangePickerField';
 export { type DateRange } from 'react-day-picker';
 
 // Charts
-export { BarChart } from './components/charts/bars/BarChart';
-export { getBarChartData, getBarChartOptions } from './components/charts/bars/bars.utils';
-export { LineChart } from './components/charts/lines/LineChart';
-export { getLineChartData, getLineChartOptions } from './components/charts/lines/lines.utils';
-export { KpiChart } from './components/charts/kpis/KpiChart';
-export { DonutChart } from './components/charts/pies/DonutChart/DonutChart';
-export { PieChart } from './components/charts/pies/PieChart/PieChart';
-export {
-  getPieChartData,
-  getPieChartOptions,
-  getDonutChartOptions,
-} from './components/charts/pies/pies.utils';
-export { TablePaginated } from './components/charts/tables/Table/TablePaginated';
+export * from './components/charts/bars/BarChart';
+export * from './components/charts/bars/bars.utils';
+export * from './components/charts/lines/LineChart';
+export * from './components/charts/lines/lines.utils';
+export * from './components/charts/kpis/KpiChart';
+export * from './components/charts/pies/DonutChart/DonutChart';
+export * from './components/charts/pies/PieChart/PieChart';
+export * from './components/charts/pies/pies.utils';
+export * from './components/charts/tables/Table/TablePaginated';
 export * from './components/charts/tables/Table/table.types';
-export { PivotTable } from './components/charts/tables/PivotTable/PivotTable';
+export * from './components/charts/tables/PivotTable/PivotTable';
 export * from './components/charts/tables/PivotTable/PivotTable.types';
-export { HeatMap } from './components/charts/tables/HeatMap/HeatMap';
+export * from './components/charts/tables/HeatMap/HeatMap';
 export * from './components/charts/tables/HeatMap/HeatMap.types';
-export { useTableGetRowsPerPage } from './components/charts/tables/Table/Table.hooks';
+export * from './components/charts/tables/Table/Table.hooks';
 
 // Chart Utils
 export { getTableTotalPages } from './components/charts/tables/Table/components/TablePagination/TablePagination';
 
 // Constants
-export { getChartColors, getChartContrastColors } from './components/charts/charts.constants';
-export { type Styles } from './styles/styles.constants';
+export * from './components/charts/charts.constants';
+export * from './styles/styles.constants';
 
 // Chartjs
 export * from './components/charts/chartjs.cartesian.constants';
 
 // Utils
-export { getStyle, getStyleNumber } from './styles/styles.utils';
+export * from './styles/styles.utils';
+export * from './utils/date.utils';
 
 // Types
 export * from './types/css.types';
