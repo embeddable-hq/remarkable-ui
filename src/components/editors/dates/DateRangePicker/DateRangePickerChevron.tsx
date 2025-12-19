f243050f1de4deff581e78f7d9e4c6a81f80afaf
mad-raccoon <ribeiro.ruifigueiredo@gmail.com>
2025-12-19 09:51:04 +0000

    RUI-137: add animations and year selection

--- src/components/editors/dates/DateRangePicker/DateRangePickerChevron.tsx
+++ src/components/editors/dates/DateRangePicker/DateRangePickerChevron.tsx
@@ -1,14 +1,20 @@
 import { IconCaretDownFilled } from '@tabler/icons-react';
 import styles from './DateRangePickerChevron.module.css';
+import clsx from 'clsx';
 
 type DateRangePickerChevronProps = {
   className?: string;
   orientation?: 'up' | 'down' | 'left' | 'right';
+  size?: number;
 };
+
+const SMALL_SIZE = 18;
 
 export const DateRangePickerChevron = ({
   orientation,
+  size,
 }: DateRangePickerChevronProps): React.JSX.Element => {
+  console.log('size', size);
   const rotation =
     orientation === 'left'
       ? 'rotate(90deg)'
@@ -16,9 +22,11 @@ export const DateRangePickerChevron = ({
         ? 'rotate(-90deg)'
         : undefined;
 
+  const isSmallChevron = size === SMALL_SIZE;
+
   return (
     <button
-      className={styles.chevron}
+      className={clsx(styles.chevron, isSmallChevron && styles.small)}
       style={{
         transform: rotation,
       }}
