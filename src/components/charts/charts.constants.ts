99530646a2801647cd919c449a8425e322986d29
Rui Ribeiro <57071299+mad-raccoon@users.noreply.github.com>
2026-01-16 08:49:22 +0000

    RUI-000: cleanup cardheader (#305)

--- src/components/charts/charts.constants.ts
+++ src/components/charts/charts.constants.ts
@@ -2,10 +2,10 @@ import { getStyle } from '../../styles/styles.utils';
 
 export const getChartColors = (): string[] => [
   getStyle('--em-sem-chart-color--1'),
+  getStyle('--em-sem-chart-color--5'),
   getStyle('--em-sem-chart-color--2'),
   getStyle('--em-sem-chart-color--3'),
   getStyle('--em-sem-chart-color--4'),
-  getStyle('--em-sem-chart-color--5'),
   getStyle('--em-sem-chart-color--6'),
   getStyle('--em-sem-chart-color--7'),
   getStyle('--em-sem-chart-color--8'),
@@ -15,7 +15,7 @@ export const getChartColors = (): string[] => [
 
 export const getChartContrastColors = (): string[] => [
   getStyle('--em-sem-chart-color--1'),
-  getStyle('--em-sem-chart-color--4'),
-  getStyle('--em-sem-chart-color--7'),
-  getStyle('--em-sem-chart-color--10'),
+  getStyle('--em-sem-chart-color--5'),
+  getStyle('--em-sem-chart-color--2'),
+  getStyle('--em-sem-chart-color--3'),
 ];
