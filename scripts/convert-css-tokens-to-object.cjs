64da7403e077fd4c0ec2250c4d35eee73096b83f
Rui Ribeiro <57071299+mad-raccoon@users.noreply.github.com>
2026-04-20 15:19:01 +0100

    EM-000: update css vars (#371)
    * update css vars
    
    * release
    
    * release
    
    * release
    
    * update styles

--- scripts/convert-css-tokens-to-object.cjs
+++ scripts/convert-css-tokens-to-object.cjs
@@ -18,9 +18,9 @@ const css = fs.readFileSync(inputPath, 'utf8');
 const lines = css.split(/\r?\n/);
 
 // Token buckets
-let core = {};
-let semantics = {};
-let component = {};
+const core = {};
+const semantics = {};
+const component = {};
 
 // Parse lines that contain CSS custom properties
 for (const rawLine of lines) {
@@ -44,23 +44,6 @@ for (const rawLine of lines) {
     component[name] = value;
   }
 }
-
-// --------------------------------------------------------
-// Overrides — Tokens that Figma exports incorrectly
-// --------------------------------------------------------
-core = {
-  ...core,
-  '--em-core-font-weight--bold': '700',
-  '--em-core-font-weight--medium': '500',
-  '--em-core-font-weight--regular': '400',
-};
-
-component = {
-  ...component,
-  '--em-selectfield-content-opacity--loading': '0.5',
-  // TODO: Get this fixed in Figma
-  '--em-tablechart-cell-max-width': 'calc(3 * var(--em-core-size--2000))',
-};
 
 // Build output file path
 const baseName = path.basename(inputPath, path.extname(inputPath));
